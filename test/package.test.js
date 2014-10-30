ca42bdc21ec2d27a2ba8a50edde867f2a96cdad4
Jared Hanson <jaredhanson@gmail.com>
2014-10-30 12:26:56 -0700

    Add test case for providing components.

--- test/package.test.js
+++ test/package.test.js
@@ -5,7 +5,14 @@ var common = require('..');
 describe('bixby-common', function() {
   
   it('should export a function', function() {
-    expect(common).to.be.an('function');
+    expect(common).to.be.a('function');
+  });
+  
+  it('should provide components', function() {
+    expect(common('entity')).to.be.a('function');
+    expect(common('logger')).to.be.a('function');
+    expect(common('notifications')).to.be.a('function');
+    expect(common('settings')).to.be.a('function');
   });
   
 });
