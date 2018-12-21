d442148e127bebab19e8931e62dbef556d303e5c
Jared Hanson <jaredhanson@gmail.com>
2018-12-21 09:21:14 -0800

    Add system settings source.

---
+++ app/settings/system.js
@@ -0,0 +1,26 @@
+exports = module.exports = function(Data) {
+  var os = require('os')
+    , path = require('path')
+    , pkginfo = require('pkginfo')
+    , decisions = require('decisions');
+  
+  
+  var ostype = os.type().toLowerCase()
+    , pkg = pkginfo.read(require.main).package
+    , dirname = '/etc'
+    , file, conf;
+  
+  
+  if (ostype.indexOf('darwin') === 0) {
+    dirname = '/Library/Preferences'
+  }
+  
+  file = decisions.resolve(path.join(dirname, pkg.name), Data.getExtensions());
+  conf = new decisions.File(file);
+  conf.open();
+  return conf;
+};
+
+exports['@require'] = [
+  'http://i.bixbyjs.org/data'
+];
