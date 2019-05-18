1bfb6118bba72f9c52a386cfae1252a6baacd866
Jared Hanson <jaredhanson@gmail.com>
2019-05-18 12:23:44 -0700

    Add services database.

--- app/services.js
+++ app/services.js
@@ -1,20 +1,27 @@
 exports = module.exports = function(IoC, logger) {
   var API = require('../lib/services/api')
+    , Database = require('../lib/services/database')
     , Registry = require('../lib/services/registry');
   
   
-  var registry = new Registry()
+  var database = new Database()
+    , registry = new Registry()
     , modules = IoC.components('http://i.bixbyjs.org/IService');
   
   return Promise.all(modules.map(function(m) { return m.create(); }))
     .then(function(impls) {
       impls.forEach(function(impl, i) {
         logger.info('Loaded service: ' + modules[i].a['@name']);
+        database.add({
+          name: modules[i].a['@name'],
+          port: modules[i].a['@port'],
+          protocol: modules[i].a['@protocol']
+        });
         registry.use(modules[i].a['@name'], impl);
       });
     })
     .then(function() {
-      return new API(registry);
+      return new API(database, registry);
     });
 }
 
